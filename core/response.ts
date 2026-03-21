e37c7efab417cc5f7af83ef22dbde66d4bb9ee7d
phantekzy <mainilotfi@gmail.com>
2026-03-21 11:00:33 +0100

    status

--- core/response.ts
+++ core/response.ts
@@ -3,7 +3,15 @@ import { ServerResponse } from "http";
 export interface ArcResponse extends ServerResponse {
   status(code: number): this;
   json(data: any): void;
-  send(data: any): void;
+  send(data: string): void;
 }
 
-export function enhanceResponse(res: ServerResponse): ArcResponse {}
+export function enhanceResponse(res: ServerResponse): ArcResponse {
+  const arcRes = res as ArcResponse;
+  arcRes.status = (code: number) => {
+    arcRes.statusCode = code;
+    return arcRes;
+  };
+
+  return arcRes;
+}
