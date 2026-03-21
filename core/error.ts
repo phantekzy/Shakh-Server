4402066c0d081511981dec3a99f63cad65b63622
phantekzy <mainilotfi@gmail.com>
2026-03-21 02:17:08 +0100

    finished creating a custom error handling system to manage request failures with precision

--- core/error.ts
+++ core/error.ts
@@ -1,3 +1,9 @@
 export class HttpError extends Error {
-
+  constructor(
+    public statusCode: number,
+    message: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
 }
