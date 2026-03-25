7f4e2eae8dfc8b3b68acc6f237a8367422b31622
phantekzy <mainilotfi@gmail.com>
2026-03-25 09:44:43 +0100

    fixed bug

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -3,9 +3,12 @@ import { Middleware } from "../core/router";
 export const cors: Middleware = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method === "OPTIONS") {
     res.status(204).end();
     return;
   }
+
   next();
 };
