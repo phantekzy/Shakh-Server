0cdcd81da0c52db8c891845d8b94e903b3c5e233
phantekzy <mainilotfi@gmail.com>
2026-03-26 14:21:13 +0100

    Streaming logic

--- middlewares/urlencodedParser.ts
+++ middlewares/urlencodedParser.ts
@@ -6,4 +6,10 @@ export const urlencodedParser: Middleware = (req, res, next) => {
   if (req.headers["content-type"] !== "application/x-www-form-urlencoded") {
     return next();
   }
+  let body = "";
+  let byteCount = 0;
+
+  req.on("data", (chunk: Buffer) => {
+    byteCount += chunk.length;
+  });
 };
