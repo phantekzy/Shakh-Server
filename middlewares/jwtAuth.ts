0fd394a2f34aca1990e21259cd171a5a80ed1fda
phantekzy <mainilotfi@gmail.com>
2026-03-26 11:08:23 +0100

    jwt authentification guard

--- middlewares/jwtAuth.ts
+++ middlewares/jwtAuth.ts
@@ -1,3 +1,7 @@
+import { HttpError } from "../core/error";
 import { Middleware } from "../core/router";
 
-export const jwtAuth: Middleware = (req, res, next) => {};
+export const jwtAuth: Middleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return next(new HttpError(401, "Unauthorized"));
+};
