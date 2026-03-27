2e078388261642db7a15ebb382b09f342dce5599
phantekzy <mainilotfi@gmail.com>
2026-03-27 06:23:19 +0100

    extracted the empty string from the HTTP request and added an empty object

--- middlewares/cookieParser.ts
+++ middlewares/cookieParser.ts
@@ -1,3 +1,6 @@
 import { Middleware } from "../core/router";
 
-export const cookieParser: Middleware = (req, res, next) => {};
+export const cookieParser: Middleware = (req, res, next) => {
+  const cookieHeader = req.headers.cookie;
+  req.cookies = {};
+};
