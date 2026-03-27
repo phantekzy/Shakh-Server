efc586637cd25fd809fbdd4939ae01b5262a08bf
phantekzy <mainilotfi@gmail.com>
2026-03-27 18:42:58 +0100

    get the client IP

--- middlewares/rateLimiter.ts
+++ middlewares/rateLimiter.ts
@@ -7,4 +7,8 @@ interface RateRecord {
 
 const store = new Map<string, RateRecord>();
 
-export const rateLimiter = (limit: number, windowMs: number): Middleware => {};
+export const rateLimiter = (limit: number, windowMs: number): Middleware => {
+  return (req, res, next) => {
+    const ip = req.socket.remoteAddress || "unknown";
+  };
+};
