478c056a2a55312d171b8a881a740967316d97a3
phantekzy <mainilotfi@gmail.com>
2026-03-27 06:48:08 +0100

    cookie parser is done

--- middlewares/cookieParser.ts
+++ middlewares/cookieParser.ts
@@ -4,6 +4,20 @@ export const cookieParser: Middleware = (req, res, next) => {
   const cookieHeader = req.headers.cookie;
   req.cookies = {};
 
-  if (!cookieHeader) return next();
+  if (!cookieHeader) {
+    return next();
+  }
+
   const cookies = cookieHeader.split(";");
+
+  for (const cookie of cookies) {
+    const parts = cookie.split("=");
+    const name = parts.shift()?.trim();
+    if (name) {
+      const value = decodeURI(parts.join("="));
+      req.cookies[name] = value;
+    }
+  }
+
+  next();
 };
