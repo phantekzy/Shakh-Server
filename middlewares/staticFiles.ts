6416fca03cf4e9ea4cf645f3913356adc772fe70
phantekzy <mainilotfi@gmail.com>
2026-03-28 14:17:29 +0100

    i have added a mime types

--- middlewares/staticFiles.ts
+++ middlewares/staticFiles.ts
@@ -1,1 +1,16 @@
+
+const MIME_TYPES : Record<string , string> = {
+    '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.json': 'application/json',
+  '.txt': 'text/plain',
+  '.svg': 'image/svg+xml',
+}
+
+
+
+
 export const staticFiles = () => {};
