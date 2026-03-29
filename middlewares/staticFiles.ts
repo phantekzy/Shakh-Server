0e00fd25f57fce74f62ce79a2c2d056a1dd8802c
phantekzy <mainilotfi@gmail.com>
2026-03-29 14:53:44 +0100

    added a path resolver to convert a relative path into a full, absolute path

--- middlewares/staticFiles.ts
+++ middlewares/staticFiles.ts
@@ -1,19 +1,25 @@
+import path from "path";
 import { Middleware } from "../core/router";
 
-const MIME_TYPES : Record<string , string> = {
-    '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'application/javascript',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.json': 'application/json',
-  '.txt': 'text/plain',
-  '.svg': 'image/svg+xml',
-}
+const MIME_TYPES: Record<string, string> = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".json": "application/json",
+  ".txt": "text/plain",
+  ".svg": "image/svg+xml",
+};
 
-
-
-
-export const staticFiles = (rootPath : string , prefix : string = '/static') : Middleware => {
-    return (req,res,next) =>{}
+export const staticFiles = (
+  rootPath: string,
+  prefix: string = "/static",
+): Middleware => {
+  const absoluteRoot = path.resolve(rootPath);
+  return (req, res, next) => {
+    if (req.method !== "GET" && req.method !== "HEAD") {
+      return next();
+    }
+  };
 };
