e484b06d0ba96b3e79f098110516725d9759bbba
phantekzy <mainilotfi@gmail.com>
2026-03-31 17:11:32 +0100

    Routes

--- server.ts
+++ server.ts
@@ -3,9 +3,12 @@ import { Arc } from "./core/app";
 import { logger } from "./middlewares/logger";
 import { cors } from "./middlewares/cors";
 import { jsonParser } from "./middlewares/jsonParser";
+import { registerUserRoutes } from "./routes/userRoutes";
 
 const app = new Arc();
 
 app.use(logger);
 app.use(cors);
 app.use(jsonParser);
+
+registerUserRoutes(app);
