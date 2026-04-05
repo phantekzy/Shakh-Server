5e30ede2de248663bc52c29adf988e207e0c694a
phantekzy <mainilotfi@gmail.com>
2026-04-05 07:31:09 +0100

    added a safety check

--- middlewares/validator.ts
+++ middlewares/validator.ts
@@ -1,5 +1,14 @@
 import { Middleware } from "../src";
 
 export const validate = (schema: Record<string, string>): Middleware => {
-  return (req, res, next) => {};
+  return (req, res, next) => {
+    const body = req.body;
+
+    if (!body || typeof body !== "object") {
+      return res.status(400).json({
+        error: "Validation Failed",
+        message: "JSON body is required.",
+      });
+    }
+  };
 };
